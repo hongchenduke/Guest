812612ff5006a988ad4b50c585f1c92534624fae
523559493@qq.com <523559493@qq.com>
2018-11-20 00:13:29 +0800

    发布会嘉宾表单增加分页器功能

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event, Guest
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello Django!")
@@ -43,4 +44,14 @@ def search_name(request):
 def guest_manage(request):
     username = request.session.get('user','')
     guest_list = Guest.objects.all()
-    return render(request,"guest_manage.html",{"user":username,"guests":guest_list})   
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        #如果不是整数,取第一个页面数据
+        contacts = paginator.page(1)
+    except EmptyPage:
+        #如果page不在范围内,取最后一个页面
+        contacts = paginator.page(Paginator.num_pages)
+    return render(request,"guest_manage.html",{"user":username,"guests":contacts})   
